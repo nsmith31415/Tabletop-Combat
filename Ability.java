a5247cc77772eb24b73154f4c94a3e1fc6f2a93f
Nick Smith <nicholas-m-smith@uiowa.edu>
2019-10-12 20:14:44 -0500

    actually did previous commit for Attack class

--- Ability.java
+++ Ability.java
@@ -3,8 +3,9 @@
  */
 public class Ability {
     private boolean limited;
+    private String casttime;
     private boolean refreshOnShort;
     private String description;
     private String damage;
-    
+
 }
--- Attack.java
+++ Attack.java
@@ -2,6 +2,7 @@
  * Attack
  */
 public class Attack {
-
-    
+    private int range;
+    private String bonusstat;
+    private string damage;
 }
