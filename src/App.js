40c361910dcbcaa6860010ac24756350a4a88ff1
Nick Smith <nicholas-m-smith@uiowa.edu>
2019-10-14 21:41:15 -0500

    implemented SCSS environment, started adding buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
@@ -8,17 +9,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Tabletop Combat!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div class="btn-holder">
+      <button class="btn btn-primary">
+        I've already created a character
+      </button>
+      <button class="btn btn-primary">
+        I'm starting from scratch
+      </button> 
+      </div>
+      
       </header>
+      
     </div>
   );
 }
