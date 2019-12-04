0f61e09da49cb7a0b712ea426cd002fcf1550d4b
nsmith31415 <nsmith31415@gmail.com>
2019-12-04 10:39:27 -0600

    Testing render

---
+++ src/App2.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require("./LogoTest2.PNG")} class="App-logo" alt="logo" />
+        <h4>
+          Welcome to Tabletop Combat!
+        </h4>
+      <div>
+        <div class="btn-holder">
+          <button class="btn btn-primary btn-flat">
+            I've already created a character
+          </button>
+        </div>
+      <div class="btn-holder">
+      <button class="btn btn-primary btn-flat" onlcick={dataInput()}>
+        I'm starting from scratch
+      </button> 
+      </div>
+      </div>
+      
+      
+      </header>
+      
+    </div>
+  );
+}
+
+function dataInput() {
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img src={require("./LogoTest2.PNG")} class="App-logo" alt="logo" />
+        <h4>
+          Welcome to Tabletop Combat!
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
